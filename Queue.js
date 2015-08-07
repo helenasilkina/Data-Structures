a8799079157ec7e7064aa097aa90c1877167c878
Helena Silkina <helenasilkina@gmail.com>
2015-08-07 09:23:53 +0300

    Update Queue.js

--- Queue.js
+++ Queue.js
@@ -1,1 +1,13 @@
-// http://algosaur.us/
+// FIFO (First In First Out) structure
+
+var Array = []
+
+function insert(element) {
+ Array.push(element);
+ console.log(Array, 'Add last element into Array');
+}
+
+function delete() {
+ Array.shift();
+ console.log(Array, 'Delete first element in Array');
+}
