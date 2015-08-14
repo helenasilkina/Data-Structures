736fa47d6d5ebf5db5d3199c247b3f0face4f155
Helena Silkina <helenasilkina@gmail.com>
2015-08-14 08:55:12 +0300

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,9 @@
+queue = []
+
+def insert(element):
+    q.append(element) #inserts element into the end of the queue
+    print q
+
+def delete():
+    q.pop(0) #removes 0th element of the queue
+    print q
