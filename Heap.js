93cb6b65c9581cfd3da4a38e2b4629883e285a36
Helena Silkina <helenasilkina@gmail.com>
2015-08-15 10:34:13 +0300

    Update Heap.js

--- Heap.js
+++ Heap.js
@@ -1,1 +1,17 @@
-// http://algosaur.us/
+var heap = [];
+var currentSize = 0;
+
+//returns parent index of ith index
+function parent(i) {
+  return i/2;
+}
+
+//returns left index of ith index
+function left(i) {
+  return 2 * i;
+}
+
+//returns right index of ith index
+function right(i) {
+  return (2 * i + 1);
+}
