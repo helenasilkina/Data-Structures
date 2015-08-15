d7484f0d5966abde38ed3408c230c6fa1b852687
Helena Silkina <helenasilkina@gmail.com>
2015-08-15 10:27:35 +0300

    Update Hash_Function.js

--- Hash_Function.js
+++ Hash_Function.js
@@ -1,1 +1,16 @@
-// http://algosaur.us/
+/*
+* Tests with:
+* hash('Color red')
+*/
+
+function hash(piece) {
+  var words = piece.split(" "),
+    colour = words[0],
+    shape = words[1],
+    hashNumber = 0;
+    for (var i = 0; i < 3; i++) {
+      hashNumber += colour[i].charCodeAt(0) - 64;
+      hashNumber += shape[i].charCodeAt(0) - 64;
+    }
+    return hashNumber;
+}
