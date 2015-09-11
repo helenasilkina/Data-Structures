455c4d0e55869051f63b1ece13b9f79b2e6de1b8
Helena Silkina <helenasilkina@gmail.com>
2015-09-11 12:28:14 +0300

    add insert, extractMax methods

--- Heap.js
+++ Heap.js
@@ -22,3 +22,38 @@ function swap(a, b) {
   heap[a] = heap[b];
   heap[b] = temp;
 }  
+
+function insert(elem) {
+  var currentSize = 0;
+  var index = heap.length;
+  var parent = parent(index);
+  var flag = 0;
+  
+  heap.push(elem);
+  currentSize += 1;
+
+  while (flag != 1) {
+    if (index == 1) {
+      flag = 1;
+    } else if (heap[par] > elem) {
+      flag = 1;
+    } else {
+      swap(parent, index);
+      index = parent;
+      parent = parent(index);
+    }  
+  }
+  
+  console.log(heap);
+  
+  function extractMax() {
+    if (currentSize != 0) {
+      var maximum = heap[1];
+      heap[1] = heap[currentSize];
+      heap.pop(currentSize); 
+      currentSize -= 1; 
+      maxHeapify(1);
+      return maximum;
+    }  
+  }    
+}
